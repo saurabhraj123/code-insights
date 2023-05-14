abe4b9ded173764d56b3258adb7531a27a6aeabb
saurabhraj123 <me4saurabh4u@gmail.com>
2023-05-14 13:21:23 +0530

    data fetching from the backend

--- pages/api/stats/[...slug].js
+++ pages/api/stats/[...slug].js
@@ -1,25 +1,25 @@
-const { Leetcode } = require('../../../utils/scraper')
+const { Leetcode } = require("../../../utils/scraper");
 
 export default async function handler(req, res) {
-    const { slug } = req.query;
-    const [site, userName] = slug;
+  const { slug } = req.query;
+  const [site, userName] = slug;
 
-    if (site === 'leetcode') {
-        try {
-            const leetcode = new Leetcode();
-            const data = await leetcode.getStats(userName);
-            console.log('data is', data);
-            return res.status(200).send(data);
-        } catch (error) {
-            return res.status(500).send('Something went wrong');
-        }
-    } else if (site === 'gfg') {
-        console.log('It is gfg');
-    } else if (site === 'github') {
-        console.log('It is github');
-    } else {
-        return res.status(404).send('invalid url');
+  if (site === "leetcode") {
+    try {
+      const leetcode = new Leetcode();
+      const data = await leetcode.getStats(userName);
+      console.log("data is", data);
+      return res.status(200).send(data);
+    } catch (error) {
+      return res.status(404).send("Leetcode profile not found.");
     }
+  } else if (site === "gfg") {
+    console.log("It is gfg");
+  } else if (site === "github") {
+    console.log("It is github");
+  } else {
+    return res.status(404).send("invalid url");
+  }
 
-    res.status(200).send(slug);
+  res.status(200).send(slug);
 }
--- utils/scraper.js
+++ utils/scraper.js
@@ -1,93 +1,157 @@
-const axios = require('axios');
+const axios = require("axios");
 
 class Leetcode {
-    async getStats(username) {
-        const query = `query getUserProfile($username: String!) { allQuestionsCount { difficulty count } matchedUser(username: $username) { contributions { points } profile { reputation ranking } submissionCalendar submitStats { acSubmissionNum { difficulty count submissions } totalSubmissionNum { difficulty count submissions } } } }`;
-        const variables = { username };
-        const response = await axios.post('https://leetcode.com/graphql/', { query, variables }, {
-            headers: {
-                'referer': `https://leetcode.com/${username}/`,
-                'Content-Type': 'application/json'
-            }
-        });
-
-        const { data } = response;
-
-        // Inspect response
-        const { errors } = data;
-        if (errors) {
-            return { error: 'user does not exist' };
-        } else {
-            return this.decodeGraphqlJson(data);
+  async getStats(username) {
+    const query = `
+  query getUserProfile($username: String!, $limit: Int!) {
+    allQuestionsCount {
+      difficulty
+      count
+    }
+    matchedUser(username: $username) {
+      contributions {
+        points
+      }
+      profile {
+        reputation
+        ranking
+      }
+      submissionCalendar
+      submitStats {
+        acSubmissionNum {
+          difficulty
+          count
+          submissions
         }
+        totalSubmissionNum {
+          difficulty
+          count
+          submissions
+        }
+      }
+    }
+    recentAcSubmissionList(username: $username, limit: $limit) {
+        id
+        title
+        titleSlug
+        timestamp
+    }
+  }
+`;
+
+    console.log("i work here but not below this");
+
+    const variables = { username, limit: 30 };
+    try {
+      const response = await axios.post(
+        "https://leetcode.com/graphql/",
+        { query, variables },
+        {
+          headers: {
+            referer: `https://leetcode.com/${username}/`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const { data } = response;
+
+      console.log("response is:", response);
+
+      // Inspect response
+      const { errors } = data;
+      if (errors) {
+        console.log("error is:", errors);
+        return { error: "user does not exist" };
+      } else {
+        return this.decodeGraphqlJson(data);
+      }
+    } catch (err) {
+      console.log("err is", err);
+    }
+  }
+
+  decodeGraphqlJson(json) {
+    let totalSolved = 0;
+    let totalQuestions = 0;
+    let easySolved = 0;
+    let totalEasy = 0;
+    let mediumSolved = 0;
+    let totalMedium = 0;
+    let hardSolved = 0;
+    let totalHard = 0;
+    let acceptanceRate = 0;
+    let ranking = 0;
+    let contributionPoints = 0;
+    let reputation = 0;
+
+    const submissionCalendar = {};
+    let recentSubmissions = {};
+
+    try {
+      console.log("data", json.data.recentAcSubmissionList);
+      const allQuestions = json.data.allQuestionsCount;
+      const matchedUser = json.data.matchedUser;
+      const submitStats = matchedUser.submitStats;
+      recentSubmissions = json.data.recentAcSubmissionList;
+      const actualSubmissions = submitStats.acSubmissionNum;
+      const totalSubmissions = submitStats.totalSubmissionNum;
+
+      // Fill in total counts
+      totalQuestions = allQuestions[0].count;
+      totalEasy = allQuestions[1].count;
+      totalMedium = allQuestions[2].count;
+      totalHard = allQuestions[3].count;
+
+      // Fill in solved counts
+      totalSolved = actualSubmissions[0].count;
+      easySolved = actualSubmissions[1].count;
+      mediumSolved = actualSubmissions[2].count;
+      hardSolved = actualSubmissions[3].count;
+
+      // Fill in etc
+      const totalAcceptCount = actualSubmissions[0].submissions;
+      const totalSubCount = totalSubmissions[0].submissions;
+      if (totalSubCount !== 0) {
+        acceptanceRate = ((totalAcceptCount / totalSubCount) * 100).toFixed(2);
+      }
+
+      contributionPoints = matchedUser.contributions.points;
+      reputation = matchedUser.profile.reputation;
+      ranking = matchedUser.profile.ranking;
+
+      const submissionCalendarJson = JSON.parse(matchedUser.submissionCalendar);
+
+      Object.keys(submissionCalendarJson).forEach((timeKey) => {
+        submissionCalendar[timeKey] = submissionCalendarJson[timeKey];
+      });
+    } catch (ex) {
+      console.log("error", ex);
+      // return StatsResponse.error('error', ex.message);
     }
 
-    decodeGraphqlJson(json) {
-        let totalSolved = 0;
-        let totalQuestions = 0;
-        let easySolved = 0;
-        let totalEasy = 0;
-        let mediumSolved = 0;
-        let totalMedium = 0;
-        let hardSolved = 0;
-        let totalHard = 0;
-        let acceptanceRate = 0;
-        let ranking = 0;
-        let contributionPoints = 0;
-        let reputation = 0;
+    // console.log('success', 'retrieved', totalSolved, totalQuestions, easySolved, totalEasy, mediumSolved, totalMedium, hardSolved, totalHard, acceptanceRate, ranking, contributionPoints, reputation, submissionCalendar)
 
-        const submissionCalendar = {};
-
-        try {
-            const allQuestions = json.data.allQuestionsCount;
-            const matchedUser = json.data.matchedUser;
-            const submitStats = matchedUser.submitStats;
-            const actualSubmissions = submitStats.acSubmissionNum;
-            const totalSubmissions = submitStats.totalSubmissionNum;
-
-            // Fill in total counts
-            totalQuestions = allQuestions[0].count;
-            totalEasy = allQuestions[1].count;
-            totalMedium = allQuestions[2].count;
-            totalHard = allQuestions[3].count;
-
-            // Fill in solved counts
-            totalSolved = actualSubmissions[0].count;
-            easySolved = actualSubmissions[1].count;
-            mediumSolved = actualSubmissions[2].count;
-            hardSolved = actualSubmissions[3].count;
-
-            // Fill in etc
-            const totalAcceptCount = actualSubmissions[0].submissions;
-            const totalSubCount = totalSubmissions[0].submissions;
-            if (totalSubCount !== 0) {
-                acceptanceRate = ((totalAcceptCount / totalSubCount) * 100).toFixed(2);
-            }
-
-            contributionPoints = matchedUser.contributions.points;
-            reputation = matchedUser.profile.reputation;
-            ranking = matchedUser.profile.ranking;
-
-            const submissionCalendarJson = JSON.parse(matchedUser.submissionCalendar);
-
-            Object.keys(submissionCalendarJson).forEach((timeKey) => {
-                submissionCalendar[timeKey] = submissionCalendarJson[timeKey];
-            });
-
-        } catch (ex) {
-            console.log('error', ex)
-            // return StatsResponse.error('error', ex.message);
-        }
-
-        // console.log('success', 'retrieved', totalSolved, totalQuestions, easySolved, totalEasy, mediumSolved, totalMedium, hardSolved, totalHard, acceptanceRate, ranking, contributionPoints, reputation, submissionCalendar)
-
-        return { totalSolved, totalQuestions, easySolved, totalEasy, mediumSolved, totalMedium, hardSolved, totalHard, acceptanceRate, ranking, contributionPoints, reputation, submissionCalendar };
-    }
+    return {
+      totalSolved,
+      totalQuestions,
+      easySolved,
+      totalEasy,
+      mediumSolved,
+      totalMedium,
+      hardSolved,
+      totalHard,
+      acceptanceRate,
+      ranking,
+      contributionPoints,
+      reputation,
+      submissionCalendar,
+      recentSubmissions,
+    };
+  }
 }
 
 // const statService = new Leetcode()
 // statService.getStats('me4saurabh4u');
 
 module.exports.Leetcode = Leetcode;
-
-
